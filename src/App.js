3c0d4b3419707b254933ed860afae7d559479b92
Joe Davitt <joseph.j.davitt@gmail.com>
2023-01-30 23:03:05 +0000

    update preloaded pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React from 'react';
 import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 // import Bootstrap from 'react-bootstrap'
 // import {
 //   BrowserRouter as Router,
@@ -16,12 +19,12 @@ import Movies from './Movies';
 
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      movies:[],
-      comments:[],
-      user:[],
+      movies: [],
+      comments: [],
+      user: [],
     }
   }
 
@@ -30,11 +33,11 @@ class App extends React.Component {
     console.log('got the movies');
     try {
       // let url = `${process.env.REACT_APP_SERVER}/movies`
-      let url =`https://api.themoviedb.org/3/movie/popular?api_key=657e3c5bf7cede69dd3283c3d78e1d03&language=en-US&page=1`
+      let url = `https://api.themoviedb.org/3/movie/popular?api_key=657e3c5bf7cede69dd3283c3d78e1d03&language=en-US&page=1`
 
       let movieData = await axios.get(url);
       console.log(movieData.data.results);
-      
+
       this.setState({
         movies: movieData.data.results,
       });
@@ -43,7 +46,7 @@ class App extends React.Component {
 
     }
   }
-  
+
   componentDidMount() {
     this.getMovies();
   }
@@ -52,23 +55,20 @@ class App extends React.Component {
     console.log('App State');
     return (
       <>
-        <Movies movies={this.state.movies}/> 
-         {/* <Router>
-          <Header />
-          <Routes>
-            <Route
-              exact path='/'
-              element={
-                <>
-                  
-                </>
-              }
 
-            >
-            </Route>
-         
-          <Footer />
-        </Router> */}
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Movies movies={this.state.movies} />
+
       </>
     )
   }
